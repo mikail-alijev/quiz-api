948693a9126a7f68a2c37db2397ac67fee774e97
Mikail Alijev <mikail.alijev@illuminetsolutions.com>
2023-08-01 13:22:24 +0100

    Player name uppercase

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -43,7 +43,7 @@ async function getAllQuizzesAsync() {
   const list = quizzes.map((quiz) => {
     return {
       id: quiz.id,
-      name: quiz.name,
+      name: quiz.name.toUpperCase(),
     };
   });
   return list;
